7e975f0bf2ef889364078f9a7669cf7be75ee2b0
Vokerlee <vokerlee@gmail.com>
2021-09-28 16:03:59 +0300

    Cache: tests are changed a bit

--- 1. Cache (2Q)/tests/launch_tests.sh
+++ 1. Cache (2Q)/tests/launch_tests.sh
@@ -4,4 +4,5 @@ time ../build/cache < 120.txt
 time ../build/cache < 1k.txt
 time ../build/cache < 10k.txt
 time ../build/cache < 100k.txt
+time ../build/cache < 500k.txt
 time ../build/cache < 1kk.txt
